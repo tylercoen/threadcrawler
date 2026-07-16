61689cf37945add122161672254f6b8dc8fee385
Tyler <tyler.coen13@gmail.com>
2026-07-16 19:15:58 -0400

    Add robots.txt parser

--- src/main/java/com/threadcrawler/Main.java
+++ src/main/java/com/threadcrawler/Main.java
@@ -1,6 +1,7 @@
 package com.threadcrawler;
 
 import com.threadcrawler.core.CrawlManager;
+import com.threadcrawler.core.RobotsManager;
 import com.threadcrawler.parser.HtmlParser;
 
 public class Main {
@@ -9,6 +10,11 @@ public class Main {
 		String startUrl = "https://en.wikipedia.org/wiki/Web_crawler";
 		int maxPages = 100;
 		int maxDepth = 3;
+
+		RobotsManager robots = new RobotsManager();
+
+		robots.loadRobots("cnn.com");
+		System.out.println(robots.getRules("cnn.com"));
 
 		HtmlParser parser = new HtmlParser();
 
